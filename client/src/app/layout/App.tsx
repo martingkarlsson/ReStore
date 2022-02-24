b513b8f6ba38b53073d2809be14466decbe190f5
Martin Karlsson <Martin.Karlsson@preciofishbone.se>
2022-02-24 07:09:21 +0100

    End of section 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,32 +1,60 @@
-import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { useState } from "react";
-import { Route, Switch } from "react-router";
-import { ToastContainer } from "react-toastify";
-import AboutPage from "../../features/about/AboutPage";
-import Catalog from "../../features/catalog/Catalog";
-import ProductDetails from "../../features/catalog/ProductDetails";
-import ContactPage from "../../features/contact/ContactPage";
-import HomePage from "../../features/home/HomePage";
-import Header from "./Header";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import AboutPage from '../../features/about/AboutPage';
+import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
+import Header from './Header';
 import 'react-toastify/dist/ReactToastify.css';
-import ServerError from "../errors/ServerError";
-import NotFound from "../errors/NotFound";
+import ServerError from '../errors/ServerError';
+import NotFound from '../errors/NotFound';
+import BasketPage from '../../features/basket/BasketPage';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 
 function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light'
+  const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea' : '#121212'
-      }
-    }
-  })
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
+      },
+    },
+  });
 
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
+
+  if (loading) return <LoadingComponent message='Initializing app...' />
 
   return (
     <ThemeProvider theme={theme}>
@@ -41,6 +69,8 @@ function App() {
           <Route path='/about' component={AboutPage} />
           <Route path='/contact' component={ContactPage} />
           <Route path='/server-error' component={ServerError} />
+          <Route path='/basket' component={BasketPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route component={NotFound} />
         </Switch>
       </Container>
